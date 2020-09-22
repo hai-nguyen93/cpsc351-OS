0fcbf93e0e8739753e9d1ceb3847cc11d0628b52
Hai Nguyen <54726078+hai-nguyen93@users.noreply.github.com>
2020-09-21 18:18:30 -0700

    Add files via upload

--- unix simple shell/unix_shell.c
+++ unix simple shell/unix_shell.c
@@ -21,7 +21,7 @@ int main(void){
         fflush(stdout);
       
         // Read command
-        int arg_num = 0;    // number of arguments        
+        int arg_num = 0;    // number of arguments    
         if (fgets(cmd, MAX_LINE, stdin) == NULL) {
             fprintf(stderr, "Error reading command.\n");
             free(last_cmd);
@@ -31,7 +31,7 @@ int main(void){
         // Look for !! (last command)
         if (cmd[0] == '!' && cmd[1] == '!' && 
             (cmd[2] == ' ' || cmd[2] == '\t' || cmd[2] == '\r' 
-             || cmd[2] == '\n' || cmd[2] == '\v' || cmd[2] == '\f')) 
+             || cmd[2] == '\n' || cmd[2] == '\v' || cmd[2] == '\f'))
         {
             if (last_cmd == NULL){
                 printf("No command in history\n");
@@ -43,7 +43,7 @@ int main(void){
         else {   // update command history    
             if (last_cmd == NULL)   last_cmd = (char*)malloc(MAX_LINE);    
             strcpy(last_cmd, cmd);
-        }
+        }       
 
         // break command into tokens         
         args[arg_num] = strtok(cmd, delim);     // get 1st token   
@@ -52,16 +52,27 @@ int main(void){
             args[arg_num] = strtok(NULL, delim);
         }
 
-        printf("Number of arguments: %d\n", arg_num);
-        for(int i = 0; i < arg_num; ++i){
-            printf("args[%d]: %s\n", i, args[i]);
-        }
-        printf("-------\n");
+        // // print tokens
+        // printf("Number of arguments: %d\n", arg_num);
+        // for(int i = 0; i < arg_num; ++i){
+        //     printf("args[%d]: %s\n", i, args[i]);
+        // }
+        // printf("-------\n");
+
+        if (arg_num == 0) continue;
 
         // Look for exit, quit
         if (strcmp(args[0], "quit") == 0 || strcmp(args[0], "exit") == 0){
             should_run = 0;
             continue;
+        }
+
+        // Look for & (background process)
+        int bg_flag = 0;
+        if (strcmp(args[arg_num-1], "&") == 0){
+            bg_flag = 1;
+            args[arg_num-1] = NULL;     // remove '&' in command
+            --arg_num;
         }
 
 
@@ -78,8 +89,13 @@ int main(void){
         }
         else{
         // * (3) parent will invoke wait() unless command included &
-            wait(NULL);
-            printf("Child complete.\n");
+            if (bg_flag == 0) {
+                waitpid(pid, NULL, 0);
+                printf("Child complete.\n");
+            }
+            else {
+                printf("pid %d running.\n", pid);
+            }
         }
     }
     
